a48719c159ff3b1ab9e585a9d596f45122a3bf87
test <test@123.com>
2017-01-18 13:59:38 +0800

    1st submission
    1st submission accepted.

--- Source/LeeCode/242_Valid_Anagram/242_Valid_Anagram.cpp
+++ Source/LeeCode/242_Valid_Anagram/242_Valid_Anagram.cpp
@@ -1,6 +1,6 @@
 // 242_Valid_Anagram.cpp : Defines the entry point for the console application.
 //Given two strings s and t, write a function to determine if t is an anagram of s.
-//
+//which means the string s and string t have same count of each charactor
 //For example,
 //s = "anagram", t = "nagaram", return true.
 //s = "rat", t = "car", return false.
@@ -19,9 +19,17 @@ using namespace std;
 class Solution1 {
 public:
     bool isAnagram(string s, string t) {
-        //int count = s
-
-
+        int sCount = s.size();
+        int tCount = t.size();
+        if (sCount != tCount) return false;
+        short result[26] = { 0 };
+        for (int i = 0; i < sCount; i++)
+            result[s[i] - 'a']++;
+        for (int i = 0; i < sCount; i++)
+            result[t[i] - 'a']--;
+        for (int i : result)
+            if (i != 0)
+                return false;
         return true;
     }
 };
@@ -31,15 +39,16 @@ int main()
     string s1 = "anagram", t1 = "nagaram"; //return true.
     string s2 = "rat", t2 = "car";//return false.
     Solution1 sl;
-    if (sl.isAnagram(s1, t1))
-        cout << "pass!" << endl;
-    else
-        cout << "failed" <<endl;
 
-    if (!sl.isAnagram(s2, t2))
-        cout << "pass!" << endl;
+    if (sl.isAnagram(s1, t1))
+        cout << "same!" << endl;
     else
-        cout << "failed" << endl;
+        cout << "diff" <<endl;
+
+    if (sl.isAnagram(s2, t2))
+        cout << "same!" << endl;
+    else
+        cout << "diff" << endl;
 
     return 0;
 }
